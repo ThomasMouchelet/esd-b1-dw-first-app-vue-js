7a65c0b03def64795b6332206355e10b825996ad
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-06-02 11:59:03 +0200

    add list films

--- src/components/FormContact.vue
+++ src/components/FormContact.vue
@@ -1,9 +1,42 @@
+<script>
+    import FilmsList from './FilmsList.vue';
 
-
+    export default {
+        name: 'FormContact',
+        components: {
+            FilmsList
+        },
+        data() {
+            return {
+                credentials: {
+                    title: '',
+                    duration: '',
+                    notation: '',
+                },
+                filmsList: [],
+            }
+        },
+        methods: {
+            handleSubmit(){
+                console.log("credentials : ", this.credentials);
+                this.filmsList.push(this.credentials);
+                this.credentials = {
+                    title: '',
+                    duration: '',
+                    notation: '',
+                }
+            }
+        },
+    }
+</script>
 
 <template>
-    <form>
-        <input type="text">
+    <form @submit.prevent="handleSubmit">
+        <input type="text" placeholder="Le titre du site" v-model="credentials.title">
+        <input type="text" placeholder="Durée du film" v-model="credentials.duration">
+        <input type="text" placeholder="Notation du film" v-model="credentials.notation">
         <input type="submit" value="Envoyer">
     </form>
+
+    <FilmsList :filmsList="filmsList" />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ContactView from "@/views/ContactView.vue"
-import AboutView from "@/views/AboutView.vue"
+import ShowFilmsView from "@/views/ShowFilmsView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: AboutView
+      component: ShowFilmsView
     },
     {
       path: '/contact',
